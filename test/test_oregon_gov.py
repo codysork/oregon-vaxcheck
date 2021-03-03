34eb60c27acec307d0949b350deeaaa859e7cc02
Cody Sork <codysork@pm.me>
2021-03-03 09:09:31 -0800

    Started work on setup.py

--- test/test_oregon_gov.py
+++ test/test_oregon_gov.py
@@ -1,9 +1,12 @@
-from bot.counties import counties_data
+from oregonvaxcheck.counties import counties_data
 
 
 class TestOregonGov:
-    def test_counties(self):
+    def test_county_data(self):
         test_county = counties_data[0]['County']
         # Make sure county data is accurate
         assert test_county == "Marion"
         assert test_county != "Orange"
+
+#    def test_chatbot_county(self):
+#        assert
